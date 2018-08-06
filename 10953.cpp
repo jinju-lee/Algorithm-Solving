cb7cdca62db8758d52ebf92d446e00df47e29857
Jinju Lee <35492329+jinju-lee@users.noreply.github.com>
2018-08-06 11:01:32 +0900

    A+B - 6

---
+++ 10953.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,a,b;
+    char s;
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        cin>>a>>s>>b;
+        cout<<a+b<<endl;
+    }
+}
