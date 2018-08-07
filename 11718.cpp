bbf35685f451d3792236e97c2867c53f151a08d2
Jinju Lee <35492329+jinju-lee@users.noreply.github.com>
2018-08-07 23:06:45 +0900

    그대로 출력하기

---
+++ 11718.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    char x;
+    while(1)
+    {
+        x=getchar();
+        if(x == -1) break;
+        putchar(x);
+    }
+   
+}
