e6f417a4ba2be7e5a65d0d930d6c1b69c2d81ca5
Jinju Lee <35492329+jinju-lee@users.noreply.github.com>
2018-08-09 17:12:39 +0900

    좌표 정렬하기

---
+++ 11650.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+#define MAX 100000  
+using namespace std;
+
+bool compare(pair<int,int>& a, pair<int,int>& b) 
+{
+	if (b.first > a.first)
+		return true;
+	else if (b.first == a.first)
+	{
+		if (b.second > a.second)
+			return true;
+		else
+			return false;
+	}
+	else
+		return false;
+}
+int main() 
+{
+	int N;
+	vector<pair<int,int>> v;
+
+	scanf("%d", &N);
+	v.resize(N);
+	
+	for (int i = 0; i < N; i++) 
+		scanf("%d %d", &v[i].first,&v[i].second);
+
+	sort(v.begin(),v.end(),compare); 
+
+	for (int i = 0; i < N; i++) 
+		printf("%d %d\n", v[i].first, v[i].second);
+}
