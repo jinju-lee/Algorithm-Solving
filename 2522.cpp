f5d64607dbadd43332acead5d599ad9477b1fe1d
Jinju Lee <35492329+jinju-lee@users.noreply.github.com>
2018-08-09 09:47:46 +0900

    별찍기 - 12

---
+++ 2522.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int num=0;
+	scanf("%d",&num);
+	for(int i=0; i<num; i++){
+		for(int k=num; k>i+1; k--){
+			printf(" ");
+		}
+		for(int j=0; j<i+1; j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+	for(int i=1; i<num; i++){
+		for(int k=0; k<i; k++){
+			printf(" ");
+		}
+		for(int j=num; j>i; j--){
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
